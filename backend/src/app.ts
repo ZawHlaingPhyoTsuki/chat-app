967a9973018cb59d5e9ad5f375fd36cd3aac7a75
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2026-01-18 21:28:02 +0700

    feat: Initialize backend with server setup, database connection, core models (User, Chat, Message), and API routes.

---
+++ backend/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+import authRoutes from "./routes/authRoute";
+import chatsRoutes from "./routes/chatRoute";
+import messagesRoutes from "./routes/messageRoute";
+import usersRoutes from "./routes/userRoute";
+
+const app = express();
+
+app.use(express.json()); // parses incoming JSON request bodies and makes them available as req.body in your route handlers
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok", message: "Server is running" });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/chats", chatsRoutes);
+app.use("/api/messages", messagesRoutes);
+app.use("/api/users", usersRoutes);
+
+export default app;
