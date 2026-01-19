6fb7d03b425cf6ad34dd628268278494fdda0bb0
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2026-01-19 22:36:36 +0700

    Merge: Integrate Clerk authentication with new routes, controllers, middleware, and a global error handler. (#2)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { clerkMiddleware } from "@clerk/express";
+import { errorHandler } from "./middleware/errorHandler";
 
 import authRoutes from "./routes/authRoute";
 import chatsRoutes from "./routes/chatRoute";
@@ -9,6 +11,8 @@ const app = express();
 
 app.use(express.json()); // parses incoming JSON request bodies and makes them available as req.body in your route handlers
 
+app.use(clerkMiddleware)
+
 app.get("/health", (req, res) => {
   res.json({ status: "ok", message: "Server is running" });
 });
@@ -17,5 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatsRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/users", usersRoutes);
+
+app.use(errorHandler);
 
 export default app;
