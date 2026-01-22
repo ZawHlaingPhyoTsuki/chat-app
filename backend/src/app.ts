7307347ef860dbfacc4a9ab6331034fb4afe7051
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2026-01-22 16:05:20 +0700

    feat: add dockerfile

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute";
 import chatsRoutes from "./routes/chatRoute";
 import messagesRoutes from "./routes/messageRoute";
 import usersRoutes from "./routes/userRoute";
+import path from "path";
 
 const app = express();
 
@@ -25,7 +26,7 @@ app.use(
 
 app.use(express.json()); // parses incoming JSON request bodies and makes them available as req.body in your route handlers
 
-app.use(clerkMiddleware)
+app.use(clerkMiddleware);
 
 app.get("/health", (req, res) => {
   res.json({ status: "ok", message: "Server is running" });
@@ -37,5 +38,14 @@ app.use("/api/messages", messagesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use(errorHandler);
+
+// serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../web/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+  });
+}
 
 export default app;
